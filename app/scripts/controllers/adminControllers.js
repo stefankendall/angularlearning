0d8d33c8b17449c21416d911a5ef6ccd5d0da235
Stefan Kendall <stefankendall@gmail.com>
2014-10-21 22:33:40 -0400

    Complete chapter 8

--- app/scripts/controllers/adminControllers.js
+++ app/scripts/controllers/adminControllers.js
@@ -1,6 +1,7 @@
 'use strict';
 angular.module('sportsStoreAdmin')
     .constant('authUrl', 'http://localhost:5500/users/login')
+    .constant('ordersUrl', 'http://localhost:5500/orders')
     .controller('authController', function ($scope, $http, $location, authUrl) {
         $scope.authenticate = function (user, pass) {
             $http.post(authUrl, {
@@ -15,15 +16,37 @@ angular.module('sportsStoreAdmin')
             });
         };
     })
-    .controller('mainController', function($scope){
+    .controller('mainController', function ($scope) {
         $scope.screens = ['Products', 'Orders'];
         $scope.current = $scope.screens[0];
 
-        $scope.setScreen = function(index){
+        $scope.setScreen = function (index) {
             $scope.current = $scope.screens[index];
         };
 
-        $scope.getScreen = function(){
+        $scope.getScreen = function () {
             return $scope.current === 'Products' ? '/views/adminProducts.html' : '/views/adminOrders.html';
         };
+    })
+    .controller('ordersController', function ($scope, $http, ordersUrl) {
+        $http.get(ordersUrl, {withCredentials: true})
+            .success(function (data) {
+                $scope.orders = data;
+            })
+            .error(function (error) {
+                $scope.error = error;
+            });
+
+        $scope.selectOrder = function (order) {
+            $scope.selectedOrder = order;
+        };
+
+        $scope.calcTotal = function (order) {
+            var total = 0;
+            for (var i = 0; i < order.products.length; i++) {
+                total += order.products[i].count * order.products[i].price;
+            }
+            return total;
+        };
+
     });
