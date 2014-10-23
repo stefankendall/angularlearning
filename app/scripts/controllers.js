ae2c4b1867f0888a5dd07f5190d1b2657f26a3b1
Stefan Kendall <stefankendall@gmail.com>
2014-10-23 12:27:23 -0400

    Partyway through 10

--- app/scripts/controllers.js
+++
@@ -1,9 +0,0 @@
-var controllersModule = angular.module('exampleApp.Controllers', []);
-
-controllersModule.controller('dayController', function ($scope, days){
-    $scope.day = days.today;
-});
-
-controllersModule.controller('tomorrowController', function ($scope, days){
-    $scope.tomorrow = days.tomorrow;
-});
--- app/scripts/filters.js
+++
@@ -1,5 +0,0 @@
-angular.module('exampleApp.Filters', []).filter('dayName', function () {
-    return function (input) {
-        return angular.isNumber(input) ? dayNames[input] : input;
-    };
-});
